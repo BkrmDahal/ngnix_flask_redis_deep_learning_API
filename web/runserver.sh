dd59ace4eda6bcf0caca92f560fdb2d915acdec7
bkrmdahal <bkrm.dahal@gmail.com>
2018-08-23 13:19:51 +0000

    bug fixed with port and echo

--- web/runserver.sh
+++ web/runserver.sh
@@ -4,4 +4,4 @@ python3 app/run_model_server.py &
 
 echo "start gunicorn server "
 # gunicorn --log-level info --log-file=/gunicorn.log --workers 4 --name app -b 0.0.0.0:8000--reload app.run_web_server:app
-gunicorn --workers 1 --name app -b 0.0.0.0:8000--reload app.run_web_server:app
+gunicorn --workers 1 --name app -b 0.0.0.0:8000 --reload app.run_web_server:app
