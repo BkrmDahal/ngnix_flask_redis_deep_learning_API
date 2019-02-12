6db62958f1687ea446c6883a653ce5ced925e508
Bikram Dahal <bkrm.dahal@gmail.com>
2019-02-12 11:14:36 +0545

    Update runserver.sh

--- web/runserver.sh
+++ web/runserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "start the deep learning model"
-python3 app/run_model_server.py &
+# python3 app/run_model_server.py &
 
 echo "start gunicorn server "
 # gunicorn --log-level info --log-file=/gunicorn.log --workers 4 --name app -b 0.0.0.0:8000--reload app.run_web_server:app
